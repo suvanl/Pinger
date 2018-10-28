da17705fa7bdc9bda16f270a388be027f5f70bf0
Suvan <mr.slrmx@gmail.com>
2018-10-28 11:44:48 +0000

    Fix dodgy formatting

--- commands/about.js
+++ commands/about.js
@@ -2,11 +2,11 @@ module.exports = {
     name: "about",
     description: "Bot latency and API response times",
     aliases: ["info", "botinfo"],
-	cooldown: 3,
-	async execute(message) {
+    cooldown: 3,
+    async execute(message) {
         message.channel.send(`
         Hey **${message.author.username}**, I'm a bot that likes to spam-ping.
         **Source code and installation instructions:** https://github.com/suvanl/Pinger
         `);
-	}
+    }
 };
